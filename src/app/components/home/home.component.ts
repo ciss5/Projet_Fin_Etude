2fca24f40bd36556b3207cd50a9839a95ec930c0
Mame Gor <mamegorciss@5gmail.com>
2025-03-07 12:28:03 +0100

    part main beginnig

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {NavbarComponent} from './navbar/navbar.component';
 import {RouterOutlet} from '@angular/router';
+import {MainComponent} from './main/main.component';
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     NavbarComponent,
-    RouterOutlet
+    RouterOutlet,
+    MainComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
