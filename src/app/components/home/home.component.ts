8a99fac8820484bc9a79e247c998d403cfdf2fba
Mame Gor <mamegorciss@5gmail.com>
2025-03-16 23:33:29 +0100

    add component footer + correction css main

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,13 +2,19 @@ import { Component } from '@angular/core';
 import {NavbarComponent} from './navbar/navbar.component';
 import {RouterOutlet} from '@angular/router';
 import {MainComponent} from './main/main.component';
+import {InfoComponent} from '../info/info.component';
+import {MarquesComponent} from '../marques/marques.component';
+import {FooterComponent} from '../footer/footer.component';
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     NavbarComponent,
     RouterOutlet,
-    MainComponent
+    MainComponent,
+    InfoComponent,
+    MarquesComponent,
+    FooterComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
