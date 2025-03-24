52c6651f68199a921f2ccfe91c0b75acc4fc161c
Mame Gor <mamegorciss@5gmail.com>
2025-03-24 01:06:46 +0100

    correction structure du code

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  content: [
-    "./src/**/*.{html,ts}", // Ajoutez tous les fichiers HTML et TypeScript dans le dossier src
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
